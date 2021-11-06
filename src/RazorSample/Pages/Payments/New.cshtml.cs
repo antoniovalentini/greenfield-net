f7bab33531ab7b4034bbd027b515a51f9a7d4b2a
avalentini <antonio.valentini@live.it>
2021-11-06 19:36:01 +0100

    new payment page

---
+++ src/RazorSample/Pages/Payments/New.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorSample.Pages.Payments
+{
+    public class New : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
