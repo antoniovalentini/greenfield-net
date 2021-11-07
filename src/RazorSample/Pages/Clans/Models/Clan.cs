4f0fcdd22e67619feaff8b4b8a9fa8e7bbd804c1
avalentini <antonio.valentini@live.it>
2021-11-07 15:17:46 +0100

    clan members

--- src/RazorSample/Pages/Clans/Models/Clan.cs
+++ src/RazorSample/Pages/Clans/Models/Clan.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace RazorSample.Pages.Clans.Models
 {
@@ -12,7 +16,42 @@ namespace RazorSample.Pages.Clans.Models
         int ClanChestLevel,
         int ClanChestMaxLevel,
         int Members,
-        IEnumerable<object> MemberList);
+        IEnumerable<ClanMember> MemberList);
 
     public record Location(long Id, string Name, bool IsCountry, string CountryCode);
+
+    public record ClanMember(
+        string Tag,
+        string Name,
+        string Role,
+        [property: JsonConverter(typeof(DateTimeConverterRoyale))] DateTime LastSeen,
+        int ExpLevel,
+        int Trophies,
+        Arena Arena,
+        int ClanRank,
+        int PreviousClanRank,
+        int Donations,
+        int DonationsReceived,
+        int ClanChestPoints);
+
+    public record Arena(long Id, string Name);
+
+    public class DateTimeConverterRoyale : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (typeToConvert != typeof(DateTime))
+            {
+                throw new Exception($"Expected type of '{typeof(DateTime)}' but found '{typeToConvert.Name}'");
+            }
+
+            var s = reader.GetString() ?? throw new InvalidOperationException();
+            return DateTime.ParseExact(s, "yyyyMMddTHHmmss.fffZ", CultureInfo.InvariantCulture);
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(CultureInfo.InvariantCulture));
+        }
+    }
 }
