f4423e4a4290dc6547a54b949a5ec22ef3892d4e
avalentini <antonio.valentini@live.it>
2021-11-07 11:42:52 +0100

    cache results

--- src/RazorSample/Pages/Clans/Index.cshtml.cs
+++ src/RazorSample/Pages/Clans/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using RazorSample.Pages.Clans.Models;
 
@@ -13,19 +14,22 @@ namespace RazorSample.Pages.Clans
     public class Index : PageModel
     {
         private readonly string _token;
+        private readonly IMemoryCache _cache;
 
         public string Error { get; private set; }
         public Clan Clan { get; private set; }
 
         private readonly JsonSerializerOptions _jsonOptions = new() {PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
 
-        public Index(IConfiguration config)
+        public Index(IConfiguration config, IMemoryCache cache)
         {
             _token = config["ApiToken"];
             if (string.IsNullOrWhiteSpace(_token))
             {
                 Error = "Api Token not found. Please define an 'ApiToken' property in the appsettings.json.";
             }
+
+            _cache = cache;
         }
 
         public async Task OnPost(string clanTag)
@@ -35,14 +39,20 @@ namespace RazorSample.Pages.Clans
                 return;
             }
 
+            if (_cache.TryGetValue(clanTag, out var cached))
+            {
+                Clan = JsonSerializer.Deserialize<Clan>((string)cached, _jsonOptions);
+                return;
+            }
+
             using var httpClient = new HttpClient
             {
                 BaseAddress = new Uri("https://api.clashroyale.com/v1/clans/")
             };
 
-            clanTag = clanTag.StartsWith("#") ? UrlEncoder.Default.Encode(clanTag) : $"%23{clanTag}";
+            var decodedClanTag = clanTag.StartsWith("#") ? UrlEncoder.Default.Encode(clanTag) : $"%23{clanTag}";
 
-            var request = new HttpRequestMessage(HttpMethod.Get, clanTag);
+            var request = new HttpRequestMessage(HttpMethod.Get, decodedClanTag);
             request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _token);
             var response = await httpClient.SendAsync(request);
             var content = await response.Content.ReadAsStringAsync();
@@ -53,6 +63,7 @@ namespace RazorSample.Pages.Clans
                 return;
             }
 
+            _cache.Set(clanTag, content);
             Clan = JsonSerializer.Deserialize<Clan>(content, _jsonOptions);
         }
     }
